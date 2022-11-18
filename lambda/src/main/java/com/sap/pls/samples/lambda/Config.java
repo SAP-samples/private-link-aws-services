14e96ac6cbfb5fcc527c30c38bbffc9dc3fd4f41
geigerj0 <112163019+geigerj0@users.noreply.github.com>
2022-11-18 12:16:42 +0100

    Add sample app for AWS Lambda (#3)

---
+++ lambda/src/main/java/com/sap/pls/samples/lambda/Config.java
@@ -0,0 +1,34 @@
+package com.sap.pls.samples.lambda;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotBlank;
+
+@Configuration
+@ConfigurationProperties(prefix = "lambda")
+@Validated
+public class Config {
+    @NotBlank
+    private String endpointUrl;
+    
+    @NotBlank
+    private String functionName;
+
+    public String getEndpointUrl() {
+        return this.endpointUrl;
+    }
+
+    public void setEndpointUrl(String endpointUrl) {
+        this.endpointUrl = endpointUrl;
+    }
+
+    public String getFunctionName() {
+        return this.functionName;
+    }
+
+    public void setFunctionName(String functionName) {
+        this.functionName = functionName;
+    }
+}
---
+++ lambda/src/main/java/com/sap/pls/samples/lambda/Controller.java
@@ -0,0 +1,34 @@
+package com.sap.pls.samples.lambda;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import software.amazon.awssdk.services.lambda.LambdaClient;
+import software.amazon.awssdk.services.lambda.model.InvokeRequest;
+
+@RestController
+public class Controller {
+
+    private Config config;
+    private LambdaClient lambdaClient;
+
+    private final Logger logger = LoggerFactory.getLogger(Controller.class);
+
+    public Controller(Config config, LambdaClient lambdaClient) {
+        this.config = config;
+        this.lambdaClient = lambdaClient;
+    }
+
+    @GetMapping
+    public String callFunction() {
+        logger.info("Calling Lambda function {}", config.getFunctionName());
+
+        InvokeRequest req = InvokeRequest.builder()
+                .functionName(config.getFunctionName())
+                .build();
+
+        return lambdaClient.invoke(req).payload().asUtf8String();
+    }
+}
